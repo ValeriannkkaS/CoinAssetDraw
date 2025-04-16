c6276b986101e85119f624fc315472fa939cd078
ValeriankaS <timonkin.max@yandex.com>
2025-04-16 10:13:55 +0300

    Написание самого базового CRUD для ползователей (практически без логики))

--- server/src/controllers/userControllers.js
+++ server/src/controllers/userControllers.js
@@ -6,15 +6,64 @@ class UserController {
             const { email, passwordHash, portfolios } = req.body;
             const user = await User.create({ email, passwordHash, portfolios });
             res.status(200).json(user);
-        } catch (e) {
-            res.status(500).json({ e });
+        } catch (err) {
+            res.status(500).json({ err });
         }
     }
-    async login(req, res) {}
-    async updateUserById(req, res) {}
-    async getAllUsers() {}
-    async getUserById(req, res) {}
-    async deleteUserById(req, res) {}
+
+    async login(req, res) {
+        //////пока что просто проставка
+        try {
+            const { email, passwordHash, portfolios } = req.body;
+            res.status(200).json({ email, passwordHash, portfolios });
+        } catch (err) {
+            res.status(500).json({ err });
+        }
+    }
+
+    async updateUserById(req, res) {
+        try {
+            const user = req.body;
+            if (!user._id) {
+                return res.status(404).json({ err: 'Post not found' });
+            }
+            const updatedUser = await User.findByIdAndUpdate(user._id, user, {
+                new: true,
+            });
+            return res.status(200).json(updatedUser);
+        } catch (err) {
+            res.status(500).json({ err });
+        }
+    }
+
+    async getAllUsers(req, res) {
+        try {
+            const users = await User.find();
+            return res.status(200).json(users);
+        } catch (err) {
+            res.status(500).json({ err });
+        }
+    }
+
+    async getUserById(req, res) {
+        try {
+            const { id } = req.params;
+            const user = await User.findById(id);
+            return res.status(200).json(user);
+        } catch (err) {
+            res.status(500).json({ err });
+        }
+    }
+
+    async deleteUserById(req, res) {
+        try {
+            const { id } = req.params;
+            const deletedUser = await User.findByIdAndDelete(id);
+            return res.status(200).json(deletedUser);
+        } catch (err) {
+            res.status(500).json({ err });
+        }
+    }
 }
 
 export default new UserController();
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -3,10 +3,11 @@ import UserController from '../controllers/userControllers.js';
 
 const userRouter = Router();
 
-userRouter.post('/', UserController.register); // первоначальная версия
-userRouter.get('/', async (req, res) => {});
-userRouter.get('/:id', async (req, res) => {});
-userRouter.put('/:id', async (req, res) => {});
-userRouter.delete('/:id', async (req, res) => {});
+userRouter.post('/auth/register', UserController.register); // первоначальная версия
+userRouter.post('/auth/login', UserController.login);
+userRouter.get('/', UserController.getAllUsers);
+userRouter.get('/:id', UserController.getUserById);
+userRouter.put('/', UserController.updateUserById);
+userRouter.delete('/:id', UserController.deleteUserById);
 
 export default userRouter;
